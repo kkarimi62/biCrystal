c0299f121fc781d5add066dcd9c479b51affdb51
kkarimi62 <kamran.karimi1362@gmail.com>
2023-06-26 08:27:55 +0200

    updates

--- simulations/simulations-ncbj-slurm.py
+++ simulations/simulations-ncbj-slurm.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
                     5:'hydrogenDiffusionInAlT1000KDislocated', 
                     6:'hydrogenDiffusionInAlBigMultipleTemps100H/temp0', #'hydrogenFree',
                     4:'mitStuff2nd', 
-                    7:'test', #'biCrystalMultipleTemp2nd/temp0', 
+                    7:'noDefect', #'biCrystalMultipleTemp2nd/temp0', 
                    }[7]
         sourcePath = os.getcwd() +\
                     {	
